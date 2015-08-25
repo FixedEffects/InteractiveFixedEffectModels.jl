440b8513d75594e525197a1b4af5f3f6bbfe6567
matthieugomez <Matthieu@dynamic-oit-fishnet-d-25.Princeton.EDU>
2015-08-25 10:25:30 -0400

    save in test

--- test/fitolsfactors.jl
+++ test/fitolsfactors.jl
@@ -12,38 +12,38 @@ precision = 2e-1
 
 for method in [:svd, :ar, :gd]
 	println(method)
-	result = fit(SparseFactorModel(:pState, :pYear, 1), Sales ~ Price, df, method =  method, maxiter = 10_000)
+	result = fit(SparseFactorModel(:pState, :pYear, 1), Sales ~ Price, df, method =  method, maxiter = 10_000, save = true)
 	@test norm(result.coef ./ [328.1653237715761, -1.0415042260420706] .- 1)  < precision
 	@test norm(abs(result.augmentdf[1, :factors1]) / 0.228 - 1) < precision
 	@test norm(abs(result.augmentdf[1, :loadings1]) / 851.514 - 1) < precision
 	@test norm(result.augmentdf[1, :residuals] / -9.7870 - 1) < precision
 
-	result = fit(SparseFactorModel(:pState, :pYear, 2), Sales ~ Price, df, method =  method, maxiter = 10_000)
+	result = fit(SparseFactorModel(:pState, :pYear, 2), Sales ~ Price, df, method =  method, maxiter = 10_000, save = true)
 	@test norm(result.coef ./ [163.01350, -0.40610] - 1) < precision
 	@test norm(abs(result.augmentdf[1, :factors1]) / 0.227 - 1) < precision
 	@test norm(abs(result.augmentdf[1, :loadings1]) / 184.1897 - 1) < precision
 	@test norm(result.augmentdf[1, :residuals]  / -1.774 - 1) < precision
 
-	result = fit(SparseFactorModel(:pState, :pYear, 2), Sales ~ Price |> pState, df, method =  method, maxiter = 1000)
+	result = fit(SparseFactorModel(:pState, :pYear, 2), Sales ~ Price |> pState, df, method =  method, maxiter = 1000, save = true)
 	@test norm(result.coef / -0.425389 - 1) < precision
 	@test norm(abs(result.augmentdf[1, :factors1])  / 0.2474 - 1) < precision
 	@test norm(abs(result.augmentdf[1, :loadings1]) / 123.460 - 1) < precision
 	@test norm(result.augmentdf[1, :residuals] / -5.222 - 1) < precision
 
-	result = fit(SparseFactorModel(:pState, :pYear, 2), Sales ~ Price |> pYear, df, method =  method, maxiter = 1000)
+	result = fit(SparseFactorModel(:pState, :pYear, 2), Sales ~ Price |> pYear, df, method =  method, maxiter = 1000, save = true)
 	@test norm(result.coef / -0.3744296120563005 -1 ) < precision
 	@test norm(abs(result.augmentdf[1, :factors1])  / 0.1918 - 1) < precision
 	@test norm(abs(result.augmentdf[1, :loadings1]) / 102.336 - 1) < precision
 	@test norm(result.augmentdf[1, :residuals] / -2.2153 - 1) < precision
 
-	result = fit(SparseFactorModel(:pState, :pYear, 2), Sales ~ Price |> pState + pYear, df, method =  method, maxiter = 1000)
+	result = fit(SparseFactorModel(:pState, :pYear, 2), Sales ~ Price |> pState + pYear, df, method =  method, maxiter = 1000, save = true)
 	@test norm(result.coef / -0.524157 - 1) < precision
 	@test norm(abs(result.augmentdf[1, :factors1]) / 0.256 - 1) < precision
 	@test norm(abs(result.augmentdf[1, :loadings1]) / 60.0481 - 1) < precision
 	@test norm(result.augmentdf[1, :residuals] / -5.614 - 1) < precision
 
 	# subset
-	result = fit(SparseFactorModel(:pState, :pYear, 2), Sales ~ Price |> pState, df, method =  method, subset = (df[:State] .<= 30), maxiter = 1000)
+	result = fit(SparseFactorModel(:pState, :pYear, 2), Sales ~ Price |> pState, df, method =  method, subset = (df[:State] .<= 30), maxiter = 1000, save = true)
 	@test size(result.augmentdf, 1) == size(df, 1)
 	@test norm(abs(result.augmentdf[:factors1][1]) /0.25965 - 1) < precision
 	@test norm(abs(result.augmentdf[:loadings1][1]) /107.832 - 1) < precision
