c5b4d86c7955b5be776078b4e23cf146806fd17a
matthieugomez <gomez.matthieu@gmail.com>
2016-09-01 18:44:48 -0400

    add cartesian

--- src/InteractiveFixedEffectModels.jl
+++ src/InteractiveFixedEffectModels.jl
@@ -9,6 +9,7 @@ module InteractiveFixedEffectModels
 
 import Distances: chebyshev
 import Base: length, copy!, axpy!, broadcast!, scale!, dot, similar, Ac_mul_B!, A_mul_B!, sumabs2!, map!, sumabs2, maxabs, fill!, norm, maxabs, size, length, eltype, rank, convert, view, clamp!, dot, vecdot
+using Base.Cartesian
 import StatsBase: coef, nobs, coeftable, vcov, predict, residuals, var, RegressionModel, model_response, stderr, confint, fit, CoefTable,  df_residual
 import DataArrays: RefArray, PooledDataVector, DataVector, PooledDataArray, DataArray
 import DataFrames: DataFrame, AbstractDataFrame, ModelMatrix, ModelFrame, Terms, coefnames, Formula, complete_cases, names!, pool
