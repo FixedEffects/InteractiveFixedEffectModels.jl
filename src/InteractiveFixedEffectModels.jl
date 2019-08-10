320d42f4036376ca02c836da3f8c91d4b9f6f97a
matthieugomez <gomez.matthieu@gmail.com>
2019-08-10 19:04:24 -0400

    remove statsmdoels

--- src/InteractiveFixedEffectModels.jl
+++ src/InteractiveFixedEffectModels.jl
@@ -16,12 +16,11 @@ using Base.Cartesian
 import StatsBase: coef, nobs, coeftable, vcov, predict, residuals, var, RegressionModel, model_response, stderror, confint, CoefTable,  dof_residual
 import CategoricalArrays: CategoricalArray, CategoricalVector, compress, categorical, CategoricalPool, levels, droplevels!
 import DataFrames: DataFrame, AbstractDataFrame, completecases, names!, ismissing
-import StatsModels: ModelMatrix, ModelFrame, Terms, coefnames, Formula, completecases, names!, @formula, evalcontrasts, check_non_redundancy!
 import LeastSquaresOptim
 using FillArrays
 using Reexport
 @reexport using FixedEffectModels
-import FixedEffectModels: title, top
+import FixedEffectModels: title, top, ModelMatrix, ModelFrame, Terms, coefnames, Formula, completecases, names!, @formula, evalcontrasts, check_non_redundancy!
 
 
 ##############################################################################
