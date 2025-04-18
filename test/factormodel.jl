e641f86248d02cb69296e88ebf371399fca62486
Matthieu Gomez <gomez.matthieu@gmail.com>
2025-04-18 16:07:43 -0400

    Update factormodel.jl

--- test/factormodel.jl
+++ test/factormodel.jl
@@ -10,7 +10,7 @@ for method in [:gauss_seidel,  :dogleg, :levenberg_marquardt]
 	@test result.augmentdf[1, :residuals] ≈ - 15.6928 atol = 1e-1
 
 	model = @formula Sales ~ 0 + ife(State, Year, 2)
-	result = regife(df, model, method = method, maxiter = 10_000,, save = true)
+	result = regife(df, model, method = method, maxiter = 10_000, save = true)
 	@test abs(result.augmentdf[1, :factors1])  ≈ 0.18662770198472406 atol = 1e-1
 	@test abs(result.augmentdf[1, :loadings1])  ≈ 587.227 atol = 1e-1
 	@test result.augmentdf[1, :residuals] ≈ 2.16611 atol = 1e-1
