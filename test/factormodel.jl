54b7ac13bbd5a2fd7c46b63c84e70c2694fe3ba5
Matthieu Gomez <gomez.matthieu@gmail.com>
2025-04-18 16:03:13 -0400

    Update factormodel.jl

--- test/factormodel.jl
+++ test/factormodel.jl
@@ -4,33 +4,33 @@ df = df = DataFrame(CSV.File(joinpath(dirname(pathof(InteractiveFixedEffectModel
 for method in [:gauss_seidel,  :dogleg, :levenberg_marquardt]
 	println(method)
 	model = @formula Sales ~ 0 + ife(State, Year, 1)
-	result = regife(df, model, method = method, maxiter = 10_000)
+	result = regife(df, model, method = method, maxiter = 10_000, save = true)
 	@test abs(result.augmentdf[1, :factors1]) ≈ 0.18662770198472406 atol = 1e-1
 	@test abs(result.augmentdf[1, :loadings1]) ≈ 587.2272 atol = 1e-1
 	@test result.augmentdf[1, :residuals] ≈ - 15.6928 atol = 1e-1
 
 	model = @formula Sales ~ 0 + ife(State, Year, 2)
-	result = regife(df, model, method = method, maxiter = 10_000)
+	result = regife(df, model, method = method, maxiter = 10_000,, save = true)
 	@test abs(result.augmentdf[1, :factors1])  ≈ 0.18662770198472406 atol = 1e-1
 	@test abs(result.augmentdf[1, :loadings1])  ≈ 587.227 atol = 1e-1
 	@test result.augmentdf[1, :residuals] ≈ 2.16611 atol = 1e-1
 
 	model = @formula Sales ~ ife(State, Year, 1) + fe(State)
-	result = regife(df, model, method = method, maxiter = 10_000)
+	result = regife(df, model, method = method, maxiter = 10_000, save = true)
 	@test abs(result.augmentdf[1, :factors1])   ≈ 0.17636 atol = 1e-1
 	@test abs(result.augmentdf[1, :loadings1])  ≈ 20.176432452716522 atol = 1e-1
 	@test result.augmentdf[1, :residuals]  ≈ - 10.0181 atol = 1e-1
 	@test result.augmentdf[1, :fe_State]  ≈ 107.4766 atol = 1e-1
 
 	model = @formula Sales ~ ife(State, Year, 2) + fe(State)
-	result = regife(df, model, method = method, maxiter = 10_000)
+	result = regife(df, model, method = method, maxiter = 10_000, save = true)
 	@test abs(result.augmentdf[1, :factors2]) ≈ 0.244 atol = 1e-1
 	@test abs(result.augmentdf[1, :loadings2]) ≈ 49.7943 atol = 1e-1
 	@test result.augmentdf[1, :residuals]  ≈ 2.165319 atol = 1e-1
 	@test result.augmentdf[1, :fe_State]  ≈ 107.47666 atol = 1e-1
 
 	model = @formula Sales ~ ife(State, Year, 2) + fe(State)
-	result = regife(df, model, subset = df.State .<= 30, method = method, maxiter = 10_000)
+	result = regife(df, model, subset = df.State .<= 30, method = method, maxiter = 10_000, save = true)
 	@test size(result.augmentdf, 1) == size(df, 1)
 	@test abs(result.augmentdf[1, :factors1])  ≈ 0.20215 atol = 1e-1
 	@test abs(result.augmentdf[1, :loadings1]) ≈ 29.546 atol = 1e-1
